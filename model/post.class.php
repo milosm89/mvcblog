f7ff609ccdde4b5c188fd53b784277477de0f2f6
milosm89 <milosmetlic89@gmail.com>
2022-09-10 13:28:45 +0200

    add ASC order to show posts

--- model/post.class.php
+++ model/post.class.php
@@ -40,7 +40,7 @@ class Post extends Dbh {
     // List all posts. 
     protected function show() {
 
-        $sql = "SELECT * FROM posts ORDER BY created_at DESC";
+        $sql = "SELECT * FROM posts ORDER BY created_at ASC";
         $stmt = $this->connect()->query($sql);
 
         if (!$stmt->execute()) {
